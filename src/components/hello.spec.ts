52382a0cfd873264af2d55620acf9b5083ca0b96
Peter Cosemans <peter.cosemans@gmail.com>
2017-07-12 21:13:14 +0200

    cleanup

--- src/components/hello.spec.ts
+++ src/components/hello.spec.ts
@@ -1,16 +1,5 @@
-import { mount, mountConfig } from '../../test/helper';
-
-// Need to import with require because there is no typings
-// available via *.vue files
-// const Hello = require('./Hello.vue');
-//
-// by using hello.vue.d.ts we can import vue files
-//
-// but....
-// TypeScript support is unreliable
-// https://github.com/vire/jest-vue-preprocessor/issues/17
-// import Hello from './hello.vue';
-
+import Vue from 'vue';
+import { mount } from 'avoriaz';
 import Hello from './hello';
 
 describe('Hello Component', () => {
@@ -18,34 +7,51 @@ describe('Hello Component', () => {
     const component = new Hello();
     expect(component.msg).toMatch('Welcome');
   });
-
-  // it('should renders correctly', () => {
-  //   // render component as a single unit
-  //   const fixture = mount(Hello);
-  //   fixture.setData({ msg: 'Hello World' });
-  //   expect(fixture).toMatchSnapshot();
-  // });
-
-  // it('should renders correctly with childs', () => {
-  //   // render component with children
-  //   const fixture = mount(Hello);
-  //   expect(fixture).toMatchSnapshot();
-  // });
-
-  // it('should renders config', () => {
-  //   const componentConfig = {
-  //     template: `
-  //       <div>
-  //         <hello ref="component"></hello>
-  //       </div>
-  //     `,
-  //     components: {
-  //       hello: Hello,
-  //     },
-  //   };
-  //   const fixture = mountConfig(componentConfig);
-  //   expect(fixture).toMatchSnapshot();
-  //   expect(fixture.vm.$refs.component).toBeTruthy();
-  //   expect(fixture.vm.$refs.component.msg).toMatch('Welcome');
-  // });
 });
+
+// This doesn't work for typescript in jest
+// Issue: TypeScript support is unreliable
+// https://github.com/vire/jest-vue-preprocessor/issues/17
+
+// hello.vue.d.ts is required to import typed vue files
+// import HelloComp from './hello.vue';
+
+// // Alternative to avoriaz mount function
+// const VueWrapper = require('avoriaz/dist/VueWrapper').default;
+// export const mountConfig = (config: any) => {
+//   const vm = new Vue(config);
+//   vm.$mount();
+//   return new VueWrapper(vm);
+// };
+
+// describe('Hello Vue Component', () => {
+//   it('should renders correctly', () => {
+//     // render component as a single unit
+//     const fixture = mount(HelloComp);
+//     fixture.setData({ msg: 'Hello World' });
+//     expect(fixture).toMatchSnapshot();
+//   });
+
+//   it('should renders correctly with childs', () => {
+//     // render component with children
+//     const fixture = mount(HelloComp);
+//     expect(fixture).toMatchSnapshot();
+//   });
+
+//   it('should renders config', () => {
+//     const componentConfig = {
+//       template: `
+//         <div>
+//           <hello ref="component"></hello>
+//         </div>
+//       `,
+//       components: {
+//         hello: HelloComp,
+//       },
+//     };
+//     const fixture = mountConfig(componentConfig);
+//     expect(fixture).toMatchSnapshot();
+//     expect(fixture.vm.$refs.component).toBeTruthy();
+//     expect(fixture.vm.$refs.component.msg).toMatch('Welcome');
+//   });
+// });
